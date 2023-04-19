8f074467c3f303b498b18b1fb2cc9aba80d15569
yves.wyder <yves.wyder@89grad.ch>
2023-04-19 22:18:11 +0200

    implement wait for db command

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -12,13 +12,29 @@ from django.test import SimpleTestCase
 
 
 @patch('core.management.commands.wait_for_db.Command.check')
+# Python BaseCommand class has a check method, \
+# that we are mocking. It adds this to the method.
 class CommandTests(SimpleTestCase):
     """Test commands."""
 
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
         patched_check.return_value = True
+        # mocking the check result
 
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(databases=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_deb_delay(self, patched_sleep, patched_check):
+        """Test waiting for database when getting OperationalError"""
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
+        # 2 times pycopg2 errors, then 3 OperationalErrors \
+        # (django), 6 times it return True
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
