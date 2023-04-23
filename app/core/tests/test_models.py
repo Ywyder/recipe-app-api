917bac711c0cd596cf6a8f4779140b7c7821d474
yves.wyder <yves.wyder@89grad.ch>
2023-04-23 22:06:17 +0200

    create django admin view

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -21,7 +21,6 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-
     def test_new_user_email_normalization(self):
         """Test email is normalized for new users """
         sample_email = [
@@ -33,3 +32,18 @@ class ModelTests(TestCase):
         for email, expected in sample_email:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'sample123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
