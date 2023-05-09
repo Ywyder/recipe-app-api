ee54d38d56a560e12e7284cef9a475ed6d4395c8
yves.wyder <yves.wyder@89grad.ch>
2023-05-09 22:15:28 +0200

    fixed linting errors

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -23,7 +23,6 @@ class PublicUserApiTests(TestCase):
     def setUp(self):
         self.client = APIClient()
 
-
     def test_create_user_success(self):
         """Test creating a user is succcessful."""
         payload = {
@@ -38,7 +37,6 @@ class PublicUserApiTests(TestCase):
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
 
-
     def test_user_with_email_exists_error(self):
         """Test error returned if user with email exists."""
         payload = {
@@ -52,7 +50,6 @@ class PublicUserApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-
     def test_password_too_short_error(self):
         """Test an error is returned if password less than 5 chars."""
         payload = {
@@ -63,5 +60,6 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.filter(email=payload['email']).exists()
+        user_exists = get_user_model().objects.\
+            filter(email=payload['email']).exists()
         self.assertFalse(user_exists)
