d4742f4b0f987bda585594fe8cd77fe314827f5c
yves.wyder <yves.wyder@89grad.ch>
2023-05-19 23:09:09 +0200

    added tests and functions for rest endpoints

--- app/user/views.py
+++ app/user/views.py
@@ -1,8 +1,12 @@
 """
 Views for the user API.
+
+type of generics APIView defines which methods are allowed on endpoints
+and shown in swagger UI
+(eg.GET when retrieveapiview, GET/POST/PATCH when RetrieveUpdateAPIView)
 """
 
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -21,3 +25,14 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for user."""
     serializer_class = AuthTokenSerializer
     render_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenicated user."""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenicated user."""
+        return self.request.user
