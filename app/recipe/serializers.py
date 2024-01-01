7ae32951da3e45ae8082056e92901181a3d151cb
Ywyder <yves.wyder@89grad.ch>
2024-01-01 21:52:00 +0100

    handling update and clearing

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -79,8 +79,8 @@ class RecipeSerializer(serializers.ModelSerializer):
             self._get_or_create_tags(tags, instance)
 
         if ingredients is not None:
-            instance.ingredient.clear()
-            self._get_or_create_tags(ingredients, instance)
+            instance.ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
