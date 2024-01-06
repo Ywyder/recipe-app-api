b57f9fb120bd63d38c61178bbc3b9d851b497b68
Ywyder <yves.wyder@89grad.ch>
2024-01-06 23:14:50 +0100

    unittest and filtering Api implemented

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,6 +1,13 @@
 """
 Views for the recipe APIs.
 """
+from drf_spectacular.utils import (
+    extend_schema_view,
+    extend_schema,
+    OpenApiParameter,
+    OpenApiTypes,
+)
+
 from rest_framework import (
     viewsets,
     mixins,
@@ -27,9 +34,23 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authenication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
+    def _params_to_ints(self, qs):
+        """Convert a list of strings to integers."""
+        return [int(str_id) for str_id in qs.split(',')]
+
     def get_queryset(self):
         """Retrieve recipes for authenticated user."""
-        return self.queryset.order_by('id')
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        if tags:
+            tags_ids = self._params_to_ints(tags)
+            # django way on how to filter on related fields
+            queryset = queryset.filter(tags__id__in=tags_ids)
+        if ingredients:
+            ingredients_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredients_ids)
+        return queryset.order_by('id').distinct()
 
     def get_serializer_class(self):
         """Return the serializer class for request. Default is detail"""
